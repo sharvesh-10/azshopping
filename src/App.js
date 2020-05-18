c53b0eeb7e57f65d5d98859dd2d29f49ea10481e
sharvesh-10 <sharveshsofficial@gmail.com>
2020-05-18 19:01:43 +0530

    developed shop page with collectio preview component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch,Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage-component';
+import ShopPage from './pages/shop/shop-component';
 
 const HatsPage =()=>(
   <div>
@@ -9,12 +10,14 @@ const HatsPage =()=>(
   </div>
 );
 
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
