9ce43bcbaf70dbd37c2f29f5a985e88e9ef01193
sharvesh-10 <sharveshsofficial@gmail.com>
2020-05-20 16:27:58 +0530

    Added button and improved sig-in page

--- src/components/sign-in/sign-in-component.jsx
+++ src/components/sign-in/sign-in-component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import FormInput from "../form-input/form-input-component";
+import CustomButton from '../custom-button/custom-button-component';
 import './sign-in-styles.scss';
 
 class SignIn extends React.Component{
@@ -24,20 +25,29 @@ class SignIn extends React.Component{
         
         return(
             <div className='sign-in'>
-                <h2>I ALREADY HAVE AN ACCOUNT</h2>
-                <span>Sign in with E-Mail</span>
+                <h1>I ALREADY HAVE AN ACCOUNT</h1>
+                <span><h2>Sign in with E-Mail</h2></span>
                 <form onSubmit={this.handleSubmit}>
-                    <input name="email" type="email" value={this.state.email} onChange={this.handleChange} required />
-                    <label>E-MAIL</label>
-                    <input 
-                        name="password" 
-                        type="password" 
-                        value={this.state.password} 
-                        onChange={this.handleChange} 
+                <label><h3>E-MAIL</h3></label>
+                    <FormInput 
+                        name="email" 
+                        type="email" 
+                        value={this.state.email} 
+                        handleChange={this.handleChange}
+                        label="email"
                         required 
                     />
-                    <label>PASSWORD</label>
-                    <input type="submit" value="Submit"/>
+                      <label><h3>PASSWORD</h3></label>
+                    <FormInput 
+                        name="password"
+                        type="password" 
+                        value={this.state.password} 
+                        handleChange={this.handleChange} 
+                        label="password"
+                        required 
+                    />
+                  
+                    <CustomButton type="submit">SignIn</CustomButton>
                 </form>
             </div>
         );
