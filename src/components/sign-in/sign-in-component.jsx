d2954dc9ef1f634364146fe848b83b987c0e36f6
sharvesh-10 <sharveshsofficial@gmail.com>
2020-05-21 11:30:52 +0530

    Added sign-in with google and connected to firebase

--- src/components/sign-in/sign-in-component.jsx
+++ src/components/sign-in/sign-in-component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FormInput from "../form-input/form-input-component";
 import CustomButton from '../custom-button/custom-button-component';
+import { signInWithGoogle } from "../../firebase/firebase-utils";
 import './sign-in-styles.scss';
 
 class SignIn extends React.Component{
@@ -48,6 +49,7 @@ class SignIn extends React.Component{
                     />
                   
                     <CustomButton type="submit">SignIn</CustomButton>
+                    <CustomButton onClick={signInWithGoogle}>Sign In with Google</CustomButton>
                 </form>
             </div>
         );
---
+++ src/firebase/firebase-utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDyJ-9GFF3Rcp7svSan1XP-3wwlJnmiHrI",
+    authDomain: "azshopping-db.firebaseapp.com",
+    databaseURL: "https://azshopping-db.firebaseio.com",
+    projectId: "azshopping-db",
+    storageBucket: "azshopping-db.appspot.com",
+    messagingSenderId: "775894049871",
+    appId: "1:775894049871:web:b6024cdf8f6807bfb9363a",
+    measurementId: "G-R0Y1W20BRB"
+};
+
+firebase.initializeApp(config);
+export const auth = firebase.auth();
+
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt:'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
