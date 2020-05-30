939ce0cb2f6a0b10bbe688f7b118be7212a4e752
sharvesh-10 <sharveshsofficial@gmail.com>
2020-05-30 11:40:32 +0530

    Added cart-dropdown with toggle facility using cart-redux

--- src/components/header/header-component.jsx
+++ src/components/header/header-component.jsx
@@ -5,9 +5,10 @@ import { ReactComponent as Logo } from "../../assets/supermarket.svg";
 /*import logo from '../../assets/logo1.png'*/
 import {auth} from '../../firebase/firebase-utils';
 import {connect} from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon-component';
+import CartDropDown from '../cart-dropdown/cart-dropdown-component';
 
-
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo />
@@ -22,12 +23,18 @@ const Header = ({currentUser}) => (
             {
                 currentUser ? <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> : <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon/>
         </div>
+        {
+            hidden ? null : <CartDropDown/>
+        }
+        
     </div>
 
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser 
+const mapStateToProps = ({user: {currentUser},cart: { hidden }}) => ({
+    currentUser,
+    hidden
 });
 export default connect(mapStateToProps)(Header);
