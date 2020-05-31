caccae41ebf601a89e6365e261362b201b9756dd
sharvesh-10 <sharveshsofficial@gmail.com>
2020-05-31 09:28:16 +0530

    Added redux code to cart-items. Grouping cart pending

--- src/redux/cart/cart-actions.js
+++ src/redux/cart/cart-actions.js
@@ -2,4 +2,6 @@ import CartActionTypes from './cart-types';
 
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
-})
+});
+
+export const addItem = item => ({ type: CartActionTypes.ADD_ITEM, payload: item });
--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,6 +1,7 @@
 import CartActionTypes from './cart-types';
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE,action) => {
@@ -8,6 +9,10 @@ const cartReducer = (state = INITIAL_STATE,action) => {
         case CartActionTypes.TOGGLE_CART_HIDDEN: return{
             ...state,
             hidden: !state.hidden
+        }
+        case CartActionTypes.ADD_ITEM: return{
+            ...state,
+            cartItems: [...state.cartItems,action.payload]
         }
         default: return state;
     }
